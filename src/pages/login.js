c1be373362d5d9aaf1728c9c16db8e47fbf86ffd
gildeir <gildeirmateus.com@gmail.com>
2021-05-20 15:01:49 -0300

    connection of page login  with app.js

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/pagesStyles/loginStyle.css'
+
+function login (){
+    return(
+        <div>
+            <h1>Olá</h1>
+        </div>
+    )
+}
+
+export default login;
